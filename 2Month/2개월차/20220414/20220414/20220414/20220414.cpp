0e3904edb092781c5e0faf9f5c3ea401eb6bea24
HyunSooA <gustn3704@naver.com>
2022-04-14 21:41:31 +0900

    20220414

---
+++ 2Month/2개월차/20220414/20220414/20220414/20220414.cpp
@@ -0,0 +1,490 @@
+// 20220414.cpp : 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include "20220414.h"
+#include <list>
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+#define MAX_LOADSTRING 100
+
+// 전역 변수:
+HINSTANCE hInst;                                // 현재 인스턴스입니다.
+WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
+WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
+
+// 이 코드 모듈에 들어 있는 함수의 정방향 선언입니다.
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: 여기에 코드를 입력합니다.
+
+    // 전역 문자열을 초기화합니다.
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_MY20220414, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // 응용 프로그램 초기화를 수행합니다.
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MY20220414));
+
+    MSG msg;
+
+    // 기본 메시지 루프입니다.
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  함수: MyRegisterClass()
+//
+//  목적: 창 클래스를 등록합니다.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_MY20220414));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_MY20220414);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
+
+   RECT title{ 0,0,1200,800 };
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+	   400, 100, //출력하고자 하는 LEFT와 TOP의 좌표
+	   title.right - title.left,
+	   title.bottom - title.top,
+	   nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  목적:  주 창의 메시지를 처리합니다.
+//
+//  WM_COMMAND  - 응용 프로그램 메뉴를 처리합니다.
+//  WM_PAINT    - 주 창을 그립니다.
+//  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
+//
+//
+
+int sNum = 3;
+RECT rc{ 550,680,600,730 };
+RECT monster{ 470, 100, 670, 200 };
+RECT monster2{ 470, 100, 650 , 190 };
+
+
+void Star(HDC hdc, int x, int y)
+{
+	MoveToEx(hdc, x+10, y, nullptr);
+	LineTo(hdc, x + 25,y - 35);
+	LineTo(hdc, x + 40, y);
+	LineTo(hdc, x+5 , y - 23);
+	LineTo(hdc, x + 45, y - 23);
+	LineTo(hdc, x+10 , y);
+}
+
+void Triangle(HDC hdc, int x, int y)
+{
+	MoveToEx(hdc, x + 5, y, nullptr);
+	LineTo(hdc, x + 25, y - 35);
+	LineTo(hdc, x + 45, y);
+	LineTo(hdc, x + 5, y);
+}
+
+
+	
+
+
+
+class Monster
+{
+private:
+	HDC hdc;
+	RECT monster;
+	bool Die;
+
+public:
+	void CreateMonster();
+	bool GetDie() { return Die; }
+	void SetDie() { Die = true; }
+
+
+
+public:
+	Monster(HDC hdc, RECT monster)
+		:hdc(hdc),monster(monster),Die(false)
+	{
+
+	}
+
+};
+
+void Monster::CreateMonster()
+{
+	if (Die == false)
+	{
+		Die = true;
+	}
+	Rectangle(hdc, monster.left, monster.top, monster.right, monster.bottom);
+	MoveToEx(hdc, monster.left, monster.top, nullptr);
+	LineTo(hdc, monster.left + 25, monster.top - 40);
+	LineTo(hdc, monster.left + 50, monster.top);
+	MoveToEx(hdc, monster.right, monster.top, nullptr);
+	LineTo(hdc, monster.right - 25, monster.top - 40);
+	LineTo(hdc, monster.right - 50, monster.top);
+	MoveToEx(hdc, monster.right, monster.top, nullptr);
+	Ellipse(hdc, monster.left + 10, monster.top + 10, monster.right - 10, monster.bottom - 10);
+	MoveToEx(hdc, monster.right, monster.top, nullptr);
+	Ellipse(hdc, monster.left + 70, monster.top + 25, monster.right - 70, monster.bottom - 25);
+}
+
+
+
+
+
+struct MYBULLET
+{
+	RECT a;
+	wchar_t btype;
+	int damage;
+};
+
+list<MYBULLET> m_BulletList;
+list<wchar_t> myMonster;
+TCHAR Hp[100];
+TCHAR mName[30];
+int hit = 0;
+
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+	case WM_CREATE:
+
+		SetTimer(hWnd, 0, 0, 0);
+		//SetTimer : 타이머를 설치하는 함수 1인자 윈도우 핸들 2인자 타이머의 id번호 여러개의 타이머가 존재할 수 있기 때문에 현재는 0번 타이머
+		//3인자 타이머 주기 기본값으로 지정, 1000당 1초 4인자 null인 경우 wm_timer라는 메세지를 발생시킴
+		break;
+
+	case WM_TIMER:
+	{
+		InvalidateRect(hWnd, 0, true);
+		
+		
+		
+
+		//InvalidateRect : 윈도우 화면 갱신 함수, 함수 호출 시 wm_paint 메세지를 발생
+		//1인자 = 갱싱할 윈도우 핸들 , 2인자 : 윈도우 안에서 갱신할 범위 지정, null인 경우 윈도우 전체 영역
+		//3인자 : false냐 true냐에 다라 화면 갱신 상태 변경
+	}
+		break;
+
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // 메뉴 선택을 구문 분석합니다.
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+			Rectangle(hdc, rc.left, rc.top, rc.right, rc.bottom);
+
+
+			TCHAR score[30];	
+			wsprintf(score ,TEXT("점수 : %d"), sNum);		
+			TextOut(hdc, 10, 10, score, lstrlen(score));
+			
+			if (myMonster.empty() == 1)
+			{
+				wchar_t boss = wchar_t("보스");
+				myMonster.push_back(boss);
+			}
+			for (auto& iter : myMonster)
+			{
+				if (iter == wchar_t("보스"))
+				{				
+					Monster m(hdc, monster);
+					m.CreateMonster();
+				}			
+			}
+			
+			SetBkColor(hdc, RGB(255, 0, 0));
+			wsprintf(Hp, TEXT("                                            "));
+			TextOut(hdc, monster.left + 10, monster.top - 60, Hp, lstrlen(Hp+hit));
+			SetBkColor(hdc, RGB(255, 255, 255));
+			if (lstrlen(Hp + hit) <= 1)
+			{
+				
+				
+			}
+
+
+			SetBkColor(hdc, RGB(0, 0, 0));
+			SetTextColor(hdc, RGB(255, 0, 255));
+			wsprintf(mName, TEXT("재간둥이"));	
+			
+			TextOut(hdc, monster.left+70, monster.top - 30, mName, lstrlen(mName));
+			
+							
+
+			random_device random;
+			mt19937 rd(random());
+			uniform_int_distribution<int> range(1, 20);
+			int rNum = range(rd);
+
+
+			if (rNum % 2 == 1)
+			{
+				if (monster.left >= rNum)
+				{
+					monster.left -= rNum;
+					monster.right -= rNum;
+				}
+			}
+			else
+			{
+				if (monster.right <= 1200 - rNum)
+				{
+					monster.left += rNum;
+					monster.right += rNum;
+				}
+			}
+			
+
+
+			for (auto iter = m_BulletList.begin(); iter != m_BulletList.end(); ++iter)
+			{
+				if (iter->btype == wchar_t("네모"))
+				{
+					Rectangle(hdc, iter->a.left + 10, iter->a.top + 10, iter->a.right - 10, iter->a.bottom - 10);
+					iter->a.top -= 10;
+					iter->a.bottom -= 10;
+					if (iter->a.top <= monster.bottom && iter->a.left <= monster.right && iter->a.right >= monster.left)
+					{
+						m_BulletList.erase(iter);
+						sNum += 1;
+						hit += 1;
+						
+						break;
+					}
+
+				}
+			
+				else if (iter->btype == wchar_t("동그라미"))
+				{
+					Ellipse(hdc, iter->a.left + 10, iter->a.top + 10, iter->a.right - 10, iter->a.bottom - 10);
+					iter->a.top -= 10;
+					iter->a.bottom -= 10;
+					if (iter->a.top <= monster.bottom && iter->a.left <= monster.right && iter->a.right >= monster.left)
+					{
+						m_BulletList.erase(iter);
+						hit += 1;
+						break;
+						
+					}
+				}
+				else if (iter->btype == wchar_t("별"))
+				{
+					Star(hdc, iter->a.left, iter->a.top);
+					iter->a.bottom -= 10;
+					iter->a.top -= 10;
+					if (iter->a.top <= monster.bottom && iter->a.left <= monster.right && iter->a.right >= monster.left)
+					{
+						m_BulletList.erase(iter);
+						hit += 1;
+						break;
+					
+					}
+					
+				}
+				else if(iter->btype == wchar_t("세모"))
+				{
+					Triangle(hdc, iter->a.left, iter->a.top);
+					iter->a.top -= 10;
+					iter->a.bottom -= 10;
+					if (iter->a.top <= monster.bottom && iter->a.left <= monster.right && iter->a.right >= monster.left)
+					{
+						m_BulletList.erase(iter);
+						hit += 1;
+						break;
+					}
+				}
+				
+				
+			}
+
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+		KillTimer(hWnd, 0);
+        PostQuitMessage(0);
+        break;
+
+	case WM_KEYDOWN:
+		switch (wParam)
+		{
+		case VK_ESCAPE:
+			PostQuitMessage(0);
+			break;
+		case VK_RIGHT:
+			rc.left += 10;
+			rc.right += 10;
+			break;
+
+		case VK_UP:
+			rc.bottom -= 10;
+			rc.top -= 10;
+			break;
+
+		case VK_LEFT:
+			rc.left -= 10;
+			rc.right -= 10;
+			break;
+
+		case VK_DOWN:
+			rc.bottom += 10;
+			rc.top += 10;
+			break;
+
+		case 'A':
+			MYBULLET A;
+			A.a = rc;
+			A.damage = 1;
+			A.btype = wchar_t("네모");
+			m_BulletList.push_back(A);
+			break;
+			
+
+		case 'S':
+			MYBULLET S;
+			S.a = rc;
+			S.damage = 2;
+			S.btype = wchar_t("세모");
+			m_BulletList.push_back(S);
+			break;
+
+
+		case 'D':
+			MYBULLET D;
+			D.a = rc;
+			D.damage = 3;
+			D.btype = wchar_t("동그라미");
+			m_BulletList.push_back(D);
+			break;
+
+
+		case 'F':
+			MYBULLET F;
+			F.a = rc;
+			F.damage = 4;
+			F.btype = wchar_t("별");
+			m_BulletList.push_back(F);
+			break;
+
+	
+			
+			break;
+		
+	
+
+
+			
+
+
+
+		}
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// 정보 대화 상자의 메시지 처리기입니다.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
---
+++ 2Month/2개월차/20220414/WindowsProject1/WindowsProject1/WindowsProject1.cpp
@@ -0,0 +1,278 @@
+﻿// WindowsProject1.cpp : 애플리케이션에 대한 진입점을 정의합니다.
+//
+
+#include "framework.h"
+#include "WindowsProject1.h"
+#include <list>
+
+using namespace std;
+
+#define MAX_LOADSTRING 100
+
+// 전역 변수:
+HINSTANCE hInst;                                // 현재 인스턴스입니다.
+WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
+WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
+
+// 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
+//함수호출 규약에 대해 공부해 오시오 __stdcall 콜러 콜리 호출한 호출원과 호출된 피호출원
+ATOM                MyRegisterClass(HINSTANCE hInstance); //창생성관련함수
+BOOL                InitInstance(HINSTANCE, int); // 주 프로그램창을 만드는 함수
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM); // 이벤트를 처리하는 함수
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM); // 쓸데없는 함수 
+//wWinMain = 문자열은 2바이트로 통일 유니코드를 사용한다.
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: 여기에 코드를 입력합니다.
+
+    // 전역 문자열을 초기화합니다.
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_WINDOWSPROJECT1, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // 애플리케이션 초기화를 수행합니다:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_WINDOWSPROJECT1));
+
+    MSG msg;
+
+    // 기본 메시지 루프입니다:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  함수: MyRegisterClass()
+//
+//  용도: 창 클래스를 등록합니다.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance) 
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WINDOWSPROJECT1));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = nullptr;
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   함수: InitInstance(HINSTANCE, int)
+//
+//   용도: 인스턴스 핸들을 저장하고 주 창을 만듭니다.
+//
+//   주석:
+//
+//        이 함수를 통해 인스턴스 핸들을 전역 변수에 저장하고
+//        주 프로그램 창을 만든 다음 표시합니다.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
+   RECT rc{ 0,0,800,600 };
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+     600, 200, //출력하고자 하는 LEFT와 TOP의 좌표
+       rc.right - rc.left,
+       rc.bottom - rc.top,
+       nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd); // 창을 갱신해라
+
+   return TRUE;
+}
+
+//
+//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  용도: 주 창의 메시지를 처리합니다.
+//
+//  WM_COMMAND  - 애플리케이션 메뉴를 처리합니다.
+//  WM_PAINT    - 주 창을 그립니다.
+//  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
+//
+//
+
+RECT rc{ 100,100,200,200 };
+//
+list<RECT> m_BulletList;
+//list<int*, int> bl;
+//RECT bl{ rc.left,rc.top,50,50 };
+
+//Ellipse bb{rc.left,rc.top,50,50 };
+
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_CREATE:
+
+        SetTimer(hWnd, 0, 0, 0);
+        //SetTimer : 타이머를 설치하는 함수 1인자 윈도우 핸들 2인자 타이머의 id번호 여러개의 타이머가 존재할 수 있기 때문에 현재는 0번 타이머
+        //3인자 타이머 주기 기본값으로 지정, 1000당 1초 4인자 null인 경우 wm_timer라는 메세지를 발생시킴
+        break;
+
+    case WM_TIMER:
+
+        InvalidateRect(hWnd, 0, true);
+        //InvalidateRect : 윈도우 화면 갱신 함수, 함수 호출 시 wm_paint 메세지를 발생
+        //1인자 = 갱싱할 윈도우 핸들 , 2인자 : 윈도우 안에서 갱신할 범위 지정, null인 경우 윈도우 전체 영역
+        //3인자 : false냐 true냐에 다라 화면 갱신 상태 변경
+
+        break;
+
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // 메뉴 선택을 구문 분석합니다:
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            //MoveToEx(hdc, 100, 100, nullptr); //여기부터 그리겠다
+            //LineTo(hdc, 200, 200); // 여기까지 그려라
+            //MoveToEx(hdc, 200, 200, nullptr);
+            //LineTo(hdc, 200, 300);
+            /*MoveToEx(hdc, 100, 100, nullptr);
+            LineTo(hdc, 200, 100);
+            LineTo(hdc, 200, 200);
+            LineTo(hdc, 100, 200);
+            LineTo(hdc, 100, 100);*/
+            
+            //사각형 그리기 함수
+            Rectangle(hdc, rc.left, rc.top,rc.right,rc.bottom);
+            for (auto& iter : m_BulletList)
+            {
+                Ellipse(hdc, iter.left, iter.top, iter.right, iter.bottom);
+                iter.top -= 10;
+                iter.bottom -= 10;
+            }
+
+            //총알을 4방향으로 쏠수있게
+            // 방향키로 w누르면 위로 d누르면 오른쪽으로 a 누르면 왼쪽으로 s누르면 아래로 쏴라
+           //Ellipse(hdc, rc.left, rc.top, 50, 50);
+            
+            
+            //원 그리기 함수
+            //Ellipse(hdc, 300, 300, 400, 400);
+            
+            // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        KillTimer(hWnd, 0);
+        PostQuitMessage(0);
+        break;
+
+    case WM_KEYDOWN:
+        switch (wParam)
+        {
+        case VK_ESCAPE:
+            PostQuitMessage(0);
+            break;
+        case VK_RIGHT:
+            rc.left += 10;
+            rc.right += 10;
+            break;
+
+        case VK_UP:
+            rc.bottom -= 10;
+            rc.top -= 10;
+            break;
+
+        case VK_LEFT:
+            rc.left -= 10;
+            rc.right -= 10;
+            break;
+
+        case VK_DOWN:
+            rc.bottom += 10;
+            rc.top += 10;
+            break;
+
+        case VK_SPACE:
+
+            m_BulletList.push_back(rc);
+            break;
+        }
+        break;
+
+    
+
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// 정보 대화 상자의 메시지 처리기입니다.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
